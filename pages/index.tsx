594d9a2b56dd400e53c306944f1b7be3b1396d8e
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-20 13:17:31 -0500

    Wiki

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,19 +25,17 @@ const Home = () => {
         Periodic Table of Elements
       </h1>
       <div className="grid grid-flow-col gap-3 text-xl">
-        <span className="font-semibold text-blue-500">Diatomic Nonmetals</span>
-        <span className="font-semibold text-cyan-500">Alkali</span>
-        <span className="font-semibold text-green-500">Alkaline Earth</span>
-        <span className="font-semibold text-orange-500">Transition</span>
-        <span className="font-semibold text-red-500">Post Transition</span>
-        <span className="font-semibold text-yellow-500">Metalloid</span>
-        <span className="font-semibold text-purple-500">Polyatomic Nonmetal</span>
-        <span className="font-semibold text-pink-500">Noble Gas</span>
-        <span className="font-semibold text-indigo-500">Lanthanide</span>
-        <span className="font-semibold text-teal-500">Actinide</span>
+        <a href='https://en.wikipedia.org/wiki/Nonmetal' target="_blank"><span className="font-semibold text-blue-500 hover:underline transition-all">Diatomic Nonmetals</span></a>
+        <a href='https://en.wikipedia.org/wiki/Alkali' target="_blank"><span className="font-semibold text-cyan-500 hover:underline transition-all">Alkali</span></a>
+        <a href='https://en.wikipedia.org/wiki/Alkaline_earth_metal' target="_blank"><span className="font-semibold text-green-500 hover:underline transition-all">Alkaline Earth</span></a>
+        <a href='https://en.wikipedia.org/wiki/Transition_metal' target="_blank"><span className="font-semibold text-orange-500 hover:underline transition-all">Transition</span></a>
+        <a href='https://en.wikipedia.org/wiki/Post-transition_metal' target="_blank"><span className="font-semibold text-red-500 hover:underline transition-all">Post Transition</span></a>
+        <a href='https://en.wikipedia.org/wiki/Metalloid' target="_blank"><span className="font-semibold text-yellow-500 hover:underline transition-all">Metalloid</span></a>
+        <a href='https://en.wikipedia.org/wiki/Polyatomic_nonmetal' target="_blank"><span className="font-semibold text-purple-500 hover:underline transition-all">Polyatomic Nonmetal</span></a>
+        <a href='https://en.wikipedia.org/wiki/Noble_gas' target="_blank"><span className="font-semibold text-pink-500 hover:underline transition-all">Noble Gas</span></a>
+        <a href='https://en.wikipedia.org/wiki/Lanthanide' target="_blank"><span className="font-semibold text-indigo-500 hover:underline transition-all">Lanthanide</span></a>
+        <a href='https://en.wikipedia.org/wiki/Actinide' target="_blank"><span className="font-semibold text-teal-500 hover:underline transition-all">Actinide</span></a>
         <span className="font-semibold text-gray-500">Unknown</span>
-
-        <a href=''><span className="font-semibold text-blue-500">Diatomic Nonmetals</span></a>
 
       </div>
       <div className="my-12">
